f52a3e4aad40000bd04d0205ef2f687949acd265
hp <saxenabhu078@gmail.com>
2019-04-12 00:21:45 +0530

    commit Settings Design

---
+++ app/src/main/java/com/e/c2cjprtechnosoft/activities/FavoriteActivity.java
@@ -0,0 +1,29 @@
+package com.e.c2cjprtechnosoft.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.e.c2cjprtechnosoft.R;
+
+public class FavoriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite);
+        if (getSupportActionBar() != null){
+
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()== android.R.id.home) {
+
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
